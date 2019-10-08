a5ad95f49f22d301fb93eec4b8bbe62a1247ba47
YukiSato <yukiyukiamam@gmail.com>
2019-10-08 11:19:08 +0900

    mail送信のお試し

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/sandbox/sandbox', to: 'sandbox#index'
-  post '/sandbox/sandbox/send', to: 'sandbox#send'
+  post '/sandbox/sandbox/send', to: 'sandbox#create'
 end
