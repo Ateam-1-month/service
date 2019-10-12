4a3324cf795225218eadddde6e72d4227b64a52d
YukiSato <yukiyukiamam@gmail.com>
2019-10-13 02:54:29 +0900

    student マイページ

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.new(user_id: 2, image: 'image_1', first_name: 'first_name_1', last_name: 'last_name_1', first_name_kana: 'first_name_kana_1', last_name_kana: 'last_name_kana_1', gender: 1, age: 12, prefecture: '岩手県', university: 'ハーバード大学', faculty: '国際学部')
+
+(1..50).each do |num|
+  Sector.create!(name: "業種#{num}")
+end
+
+(1..50).each do |num|
+  WorkContent.create!(name: "業務内容#{num}")
+end
