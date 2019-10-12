11b789501b78d662aaaef8bdfb3b6bac580f2cf9
s1250023-Igarashi <s1250023@u-aizu.ac.jp>
2019-10-13 01:39:53 +0900

    add relation

--- src/app/models/student.rb
+++ src/app/models/student.rb
@@ -1,2 +1,10 @@
 class Student < ApplicationRecord
+  belongs_to :user
+  has_many :appeals
+  has_many :careers
+  has_many :skills
+  has_many :student_sectors
+  has_many :sectors, through: :student_sectors
+  has_many :student_work_contents
+  has_many :work_contents, through: :student_work_contents
 end
