d6a949cdb37968c9812e9146993ffaf514234aec
YukiSato <yukiyukiamam@gmail.com>
2019-10-13 23:24:12 +0900

    ログインの処理

--- src/app/controllers/sessions_controller.rb
+++ src/app/controllers/sessions_controller.rb
@@ -8,7 +8,20 @@ class SessionsController < ApplicationController
       if user.activated
         log_in user
         flash[:success] = 'ログインしました.'
-        redirect_to '/users/new'
+        if user.company || user.student
+          if user.is_student
+            redirect_to '/posts/index'
+          else
+            redirect_to "/companies/#{user.company.id}"
+          end
+        else
+          flash[:success] = 'プロフィール登録が完了していません.プロフィールを登録してください.'
+          if user.is_student
+            redirect_to '/students/new'
+          else
+            redirect_to '/companies/new'
+          end
+        end
       else
         flash[:success] = 'まだ本登録ができていません.'
         redirect_to '/login'
