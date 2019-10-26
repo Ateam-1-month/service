4f7cc6458278f36c4ea5dc189122b3b45019cb35
YukiSato <yukiyukiamam@gmail.com>
2019-10-26 12:55:13 +0900

    最終調整

--- src/app/javascript/login.js
+++ src/app/javascript/login.js
@@ -4,14 +4,14 @@ $(function(){
         $('.student-contents').addClass('blur');
         $('.header-container').addClass('blur');
         $('.footer').addClass('blur');
-        $('.top-image').addClass('blur');
+        $('.top-container').addClass('blur');
     });
     $('.login-cancel').on('click',function(){
         $('.login-modal-container').addClass('modal-hidden');
         $('.student-contents').removeClass('blur');
         $('.header-container').removeClass('blur');
         $('.footer').removeClass('blur');
-        $('.top-image').removeClass('blur');
+        $('.top-container').removeClass('blur');
     });
     $(window).scroll(function() {
         $('.alert-success').addClass('modal-hidden');
